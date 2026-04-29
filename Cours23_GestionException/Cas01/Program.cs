19144120f9bc014a0bd83c199dc80700f3425959
2383152 <2383152@L121-24-13188>
2026-04-29 09:41:47 -0400

    hh

---
+++ Cours23_GestionException/Cas01/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Quic;
+
+namespace Cas01_TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exemple 1: Divion de deux nombre
+
+            try
+            {
+                int a = 10;
+                int b = 6;
+
+                int resultat = a / b; //Division par 0
+
+                Console.WriteLine(resultat);
+            }
+            catch(DivideByZeroException execption)
+            {
+                Console.WriteLine(execption.Message);
+            }
+
+            //Exemple 2: wrong entry
+
+            try
+            {
+                Console.Write("Entrez un nombre: ");
+                int nombre = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("nombre entré: " + nombre);
+            }
+            catch(FormatException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            //Exemple 3: fichier introuvable
+
+            try
+            {
+                StreamReader reader = new StreamReader("donnees,txt");
+                string contenu = reader.ReadToEnd(); //reader.ReadToEnd(); = lecture jusqu'à la fin du fichier et retourne le contenu sous forme de chaine de caractère
+                Console.WriteLine(contenu);
+            }
+            catch(FileNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            
+        }
+    }
+}
